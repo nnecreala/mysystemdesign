c0344ddb4fdacff85c250a4f29089594882fb904
nnicoleta <nicoleta.necreala@gmail.com>
2026-02-12 15:02:15 +0200

    Add docker and maven and spring boot 4 to the project

---
+++ notification-service/src/main/java/com/mysystemdesign/notification/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.mysystemdesign.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
